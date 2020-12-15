fa6197676e45981af6cca7d1981893c0fada59f8
mansoo <i3rewmaster@gmail.com>
2020-12-15 23:29:34 +0900

    add config

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key')
+const { User } = require("./model/User");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://mumble:abcd1234@cluster0.t9k5a.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -14,5 +20,19 @@ mongoose.connect('mongodb+srv://mumble:abcd1234@cluster0.t9k5a.mongodb.net/test?
 
 
 app.get('/', (req,res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
